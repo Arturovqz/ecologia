b2ab31cf241bec39174c5a945049169047df42ef
Arturovqz <313428ar@gmail.com>
2025-12-11 00:35:45 -0600

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("scroll", () => {
-    let scrollTop = document.documentElement.scrollTop;
-    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    let progress = (scrollTop / height) * 100;
-    document.getElementById("progressBar").style.width = progress + "%";
-});
